ac872b1a64ce538930b7ad9108c45a125d6da945
Tayri9 <tayrifernandez@gmail.com>
2022-09-24 19:26:10 +0100

    commit 3
    tiempo de partida

--- Assets/Scripts/DetectorMeta.cs
+++ Assets/Scripts/DetectorMeta.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DetectorMeta : MonoBehaviour
 {
     [SerializeField]
     GameObject pantallaFinal;
+
+    [SerializeField]
+    TextMeshProUGUI textLabelTime;
+
+    float tiempoPartida = 0.0f;
+    bool estaJugando = true;
+
+    private void Update()
+    {
+        if (estaJugando == true)
+        {
+            tiempoPartida = tiempoPartida + Time.deltaTime;
+        }        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,6 +30,10 @@ public class DetectorMeta : MonoBehaviour
             pantallaFinal.SetActive(true);
             //Bloquear jugador para que no se mueva al llegar a la meta
             other.GetComponent<MovimientoJugador>().enabled = false;
+            estaJugando = false;
+            textLabelTime.text = tiempoPartida.ToString();            
         }
     }
+
+    
 }
