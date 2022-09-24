cab2e00489ebeedc980a1ca3c73e2587f9b4d7c5
Tayri9 <tayrifernandez@gmail.com>
2022-09-24 18:54:58 +0100

    commit 2
    Pantalla fin

--- Assets/Scripts/DetectorMeta.cs
+++ Assets/Scripts/DetectorMeta.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class DetectorMeta : MonoBehaviour
 {
+    [SerializeField]
+    GameObject pantallaFinal;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             Debug.Log("Jugador llegó a la meta");
+            pantallaFinal.SetActive(true);
+            //Bloquear jugador para que no se mueva al llegar a la meta
+            other.GetComponent<MovimientoJugador>().enabled = false;
         }
     }
 }
